273282634df0768cc9b09aa29f81832e6a4cb839
acalvom <andreac.3108@gmail.com>
2023-12-11 16:46:01 +0100

    build: install & setup react-router

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 export const App = () => {
   return (
     <div className="bg-grey-light p-4">
-      <h1 className="text-4xl font-bold">Oompa Loompa Crew</h1>
+      <h1 className="text-4xl font-bold">Oompa Loompa Crew Home</h1>
     </div>
   )
 }
