9d6e81528db415852edd4004b205617190472fe0
acalvom <andreac.3108@gmail.com>
2023-12-11 16:21:04 +0100

    build: install & setup tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'success-light': '#f0fe58',
+        'success-main': '#c3dc06',
+        'success-dark': '#a5be00',
+        'warning-light': '#ffc36d',
+        'warning-main': '#ff9e32',
+        'warning-dark': '#cc4902',
+        'error-light': '#ff8b8b',
+        'error-main': '#ff4949',
+        'error-dark': '#be0000',
+        'grey-light': '#ced4da',
+        'grey-main': '#adb5bd',
+        'grey-dark': '#6c757d',
+      },
+    },
+  },
+  plugins: [],
+}
