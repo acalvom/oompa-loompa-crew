a37c9d87c70f03630527ec68a800c2424c1f6b4e
acalvom <andreac.3108@gmail.com>
2023-12-13 16:29:43 +0100

    style: add vertical margin to Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,5 @@
 export const Main = ({ children }) => {
-  return <main className="flex flex-col mx-8 sm:mx-24">{children}</main>
+  return <main className="flex flex-col mx-8 my-4 sm:mx-24">{children}</main>
 }
 
 export default Main
