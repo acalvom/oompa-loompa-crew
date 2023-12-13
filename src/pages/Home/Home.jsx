d96a27e68da47753a997fefb94f09677787a9126
acalvom <andreac.3108@gmail.com>
2023-12-13 16:08:51 +0100

    feat: create Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,46 @@
+import { Layout } from '@/layout'
+import { useAppDispatch, useAppSelector } from '@/hooks/store'
+import { decrement, increment } from '@/redux/counter/counterSlice'
+
+export const Home = () => {
+  const dispatch = useAppDispatch()
+  const count = useAppSelector((state) => state.counter.value)
+
+  return (
+    <Layout>
+      <div className="bg-grey-light p-4 my-4">
+        <h1 className="text-4xl font-bold">Oompa Loompa Crew Home</h1>
+      </div>
+      <div className="rounded border-2 border-yellow-500">
+        <div className="flex flex-row p-2 gap-2 justify-center">
+          <button
+            className="bg-red-500	rounded-sm p-2"
+            aria-label="Decrement value"
+            data-testid="decrement"
+            onClick={() => dispatch(decrement())}
+          >
+            - Decrement
+          </button>
+          <button
+            className="bg-lime-500	rounded-sm p-2"
+            aria-label="Increment value"
+            data-testid="increment"
+            onClick={() => dispatch(increment())}
+          >
+            + Increment
+          </button>
+        </div>
+        <div className="flex justify-center my-2">
+          <span
+            data-testid="count"
+            className="text-xl font-bold text-center rounded bg-cyan-500 p-2 w-1/4"
+          >
+            {count}
+          </span>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Home
