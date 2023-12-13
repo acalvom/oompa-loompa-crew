e550741e2e7cb5431d520db0c509d66871142cb3
acalvom <andreac.3108@gmail.com>
2023-12-13 17:34:59 +0100

    test: add tests for NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -7,14 +7,23 @@ export const NotFound = () => {
     <Layout>
       <div className="flex flex-col md:flex-row gap-4">
         <div className="h-2/3 md:max-h-full flex justify-center">
-          <img className="h-full" src={willyWonka} alt="Willy Wonka" />
+          <img
+            className="h-full"
+            src={willyWonka}
+            alt="Willy Wonka"
+            data-testid="willy-wonka-img"
+          />
         </div>
-        <div className="flex flex-col  items-center md:items-end md:justify-around">
+        <div className="flex flex-col items-center md:items-end md:justify-around">
           <h1 className="text-4xl md:text-8xl font-bold my-4">Oops!</h1>
           <p className="text-2xl md:text-4xl font-semibold my-4 md:text-right">
             This page does not exist
           </p>
-          <Link to="/" className="rounded py-2 px-4 bg-grey-light font-semibold">
+          <Link
+            to="/"
+            className="rounded py-2 px-4 bg-grey-light font-semibold"
+            data-testid="back-to-home"
+          >
             Back with the Oompas
           </Link>
         </div>
