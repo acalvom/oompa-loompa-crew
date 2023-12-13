157a8e955ae8738f33d713c4b5ad6617d413dca3
acalvom <andreac.3108@gmail.com>
2023-12-13 16:05:44 +0100

    feat: create Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import oompaLogo from '@/assets/logo-oompa.png'
+
+export const Header = () => {
+  return (
+    <header>
+      <nav className="bg-grey-light px-8 sm:px-24 py-3">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link to="/" className="flex items-center">
+            <img src={oompaLogo} className="mr-4 h-7 sm:mr-8 sm:h-9" alt="Oompa Loompa Logo" />
+            <span className="self-center text-md sm:text-xl font-bold whitespace-nowrap">
+              Oompa Loompas' Crew
+            </span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
