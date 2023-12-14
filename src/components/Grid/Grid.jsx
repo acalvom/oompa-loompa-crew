c89ce5e61309db57c1797cedbf11e47c47c6f0b9
acalvom <andreac.3108@gmail.com>
2023-12-14 13:34:03 +0100

    feat: create Grid component

---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,22 @@
+import { Card } from '../Card'
+
+export const Grid = ({ oompas }) => {
+  return (
+    <div className="flex items-center justify-center">
+      {oompas.length === 0 ? (
+        <h3 className="text-3xl mb-8 pt-8 font-bold text-center text-grey-dark border-t-2">
+          Where are the Oompas? 🔦 <br />
+          We can't find them 🌚 🌝
+        </h3>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:w-full">
+          {oompas.map((oompa) => (
+            <Card oompa={oompa} key={oompa.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Grid
