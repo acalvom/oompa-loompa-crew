a25ee1b09b8dc9b60857b151c590aed2044d79cd
acalvom <andreac.3108@gmail.com>
2023-12-17 11:48:10 +0100

    test: config env in cypress

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -11,6 +11,9 @@ export default defineConfig({
     runMode: 2,
     openMode: 2,
   },
+  env: {
+    apiUrl: 'https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas'
+  },
   e2e: {
     testIsolation: true,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
