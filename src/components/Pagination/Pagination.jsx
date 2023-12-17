5e0c284436a09bd85c69366116232b40b9510f6c
acalvom <andreac.3108@gmail.com>
2023-12-17 11:05:07 +0100

    test: add Pagination component test

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,10 +1,6 @@
 import { useAppDispatch, useAppSelector } from '@/hooks/store'
 import { setNextPage, setPreviousPage } from '@/redux/pagination/paginationSlice'
 
-// TODO: Cypress test
-// TODO: Button tests
-// TODO: Loading tests
-
 export const Pagination = () => {
   const dispatch = useAppDispatch()
   const { current: page, total } = useAppSelector((state) => state.pagination)
@@ -14,7 +10,7 @@ export const Pagination = () => {
 
   return (
     <>
-      <span className="text-grey-dark uppercase text-right sm:hidden">
+      <span className="text-grey-dark uppercase text-right sm:hidden" data-testid="page-label-sm">
         Page {page} of {total}
       </span>
       <div className="flex flex-row justify-between pt-8 mb-4 border-t-2 border-grey-dark">
@@ -22,16 +18,18 @@ export const Pagination = () => {
           className="min-w-115 bg-grey-dark text-white hover:bg-grey-light hover:text-grey-dark disabled:bg-gray-200  disabled:text-gray-500  font-bold py-2 px-4 rounded uppercase"
           onClick={previousPage}
           disabled={page === 1}
+          data-testid='previous-button'
         >
           Previous
         </button>
-        <span className=" hidden sm:text-grey-dark sm:uppercase sm:block">
+        <span className=" hidden sm:text-grey-dark sm:uppercase sm:block" data-testid="page-label">
           Page {page} of {total}
         </span>
         <button
           className="min-w-115 bg-grey-dark text-white hover:bg-grey-light hover:text-grey-dark disabled:bg-gray-200  disabled:text-gray-500  font-bold py-2 px-4 rounded uppercase"
           onClick={nextPage}
           disabled={page === total}
+          data-testid='next-button'
         >
           Next
         </button>
