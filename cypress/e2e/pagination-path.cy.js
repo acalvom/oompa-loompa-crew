6063fd1311c9e6b104ccd4f194637ae67a2b420e
acalvom <andreac.3108@gmail.com>
2023-12-17 11:56:27 +0100

    test: add pagination path test

---
+++ cypress/e2e/pagination-path.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+describe('Pagination path', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: `${Cypress.env('apiUrl')}?page=1`,
+      },
+      { fixture: 'oompasPageOne.json' }
+    ).as('getOompasPage1')
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: `${Cypress.env('apiUrl')}?page=2`,
+      },
+      { fixture: 'oompasPageTwo.json' }
+    ).as('getOompasPage2')
+
+    cy.visit('http://localhost:3000')
+    cy.wait('@getOompasPage1')
+  })
+
+  it('Canot move to Previous page in first page', () => {
+    cy.getBySel('oompa-grid').children().should('have.length', 25)
+    cy.getBySel('previous-button').should('be.visible').and('be.disabled')
+  })
+
+  it('Move to Next page in first page', () => {
+    cy.getBySel('next-button').should('be.visible').and('not.be.disabled').click()
+    cy.wait('@getOompasPage2')
+    cy.getBySel('previous-button').should('be.visible').and('not.be.disabled')
+  })
+
+  it('Move to Previous page in second page', () => {
+    cy.getBySel('next-button').should('be.visible').and('not.be.disabled').click()
+    cy.wait('@getOompasPage2')
+    cy.getBySel('previous-button').s
+    cy.getBySel('previous-button').should('be.visible').and('not.be.disabled').click()
+    cy.wait('@getOompasPage1')
+    cy.getBySel('previous-button').should('be.visible').and('be.disabled')
+  })
+})
