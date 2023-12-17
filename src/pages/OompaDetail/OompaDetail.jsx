5063f4b8e56216865a41e85ac2172dc5eefd43e2
acalvom <andreac.3108@gmail.com>
2023-12-17 20:09:37 +0100

    chore: refactor with DetailsCard

--- src/pages/OompaDetail/OompaDetail.jsx
+++ src/pages/OompaDetail/OompaDetail.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import { getOompaByID } from '@/services'
-import { getGenderLabel } from '@/utils/gender-label'
 import { Layout } from '@/layout'
 import { Loading } from '@/components/Loading'
-import { parseHtml } from '@/utils/html-parser'
+import { DetailCard } from '@/components/DetailCard'
+import { BackButton } from '@/components/BackButton'
 
 export const OompaDetail = () => {
   const { oompaId } = useParams()
-
   const [isLoading, setIsLoading] = useState(false)
   const [oompa, setOompa] = useState({
     first_name: '',
@@ -32,41 +31,16 @@ export const OompaDetail = () => {
   }
 
   useEffect(() => {
-    getOompa().then((data) => {
-      setOompa(data)
-    })
+    getOompa().then((data) => setOompa(data))
   }, [oompaId])
 
   return (
     <Layout>
       {isLoading && <Loading />}
       {!isLoading && (
-        <div className="flex flex-col ">
-          <div className="flex flex-col lg:flex-row w-full lg:max-h-400 gap-4">
-            <img
-              className="sm:max-h-400 lg:w-1/2 object-cover"
-              src={oompa.image}
-              alt={oompa.first_name}
-              data-testid="oompa-img"
-            />
-            <div className=" lg:w-1/2">
-              <div className="font-semibold text-xl mb-1">
-                {oompa.first_name} {oompa.last_name}
-              </div>
-              <p className="text-sm text-grey-dark mb-0.5">{getGenderLabel(oompa.gender)}</p>
-              <p className="text-sm text-grey-dark mb-0.5 italic" data-testid="oompa-profession">
-                {oompa.profession}
-              </p>
-              <div className="my-5 leading-relaxed">{parseHtml(oompa.description)}</div>
-            </div>
-          </div>
-          <Link
-            to="/"
-            className="rounded py-2 px-4 bg-grey-light font-semibold mt-4 w-250 text-center"
-            data-testid="back-to-home"
-          >
-            Back with the Oompas
-          </Link>
+        <div className="flex flex-col">
+          <DetailCard oompa={oompa} />
+          <BackButton />
         </div>
       )}
     </Layout>
