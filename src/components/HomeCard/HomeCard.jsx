b1fa5bc112f45d345e617bdbed587a2c86add081
acalvom <andreac.3108@gmail.com>
2023-12-17 19:38:42 +0100

    chore: destrucutre object

--- src/components/HomeCard/HomeCard.jsx
+++ src/components/HomeCard/HomeCard.jsx
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom'
 
 export const HomeCard = (props) => {
   const { oompa } = props
+  const { id, image, first_name, last_name, gender, profession } = oompa
 
   return (
-    <div className="p-1" key={oompa.id}>
-      <Link to={`/oompa/${oompa.id}`}>
-        <img src={oompa.image} alt={oompa.first_name} className="w-full max-h-64 object-cover" />
+    <div className="p-1" key={id}>
+      <Link to={`/oompa/${id}`}>
+        <img src={image} alt={first_name} className="w-full max-h-64 object-cover" />
         <div className="py-4">
           <div className="font-semibold text-xl mb-1">
-            {oompa.first_name} {oompa.last_name}
+            {first_name} {last_name}
           </div>
-          <p className="text-sm text-grey-dark mb-0.5">{getGenderLabel(oompa.gender)}</p>
+          <p className="text-sm text-grey-dark mb-0.5">{getGenderLabel(gender)}</p>
           <p className="text-sm text-grey-dark mb-0.5 italic" data-testid="oompa-profession">
-            {oompa.profession}
+            {profession}
           </p>
         </div>
       </Link>
