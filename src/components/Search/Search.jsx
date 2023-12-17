b0f2e7bbff949d3e691f733b3bd3404167488320
acalvom <andreac.3108@gmail.com>
2023-12-17 14:14:22 +0100

    feat: add filter serach in Home page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,13 @@
 import search from '@/assets/search.png'
 
-export const Search = () => {
+export const Search = ({setSearch}) => {
   return (
     <div className="flex flex-row relative w-56 my-3 mr-0 ml-auto">
       <input
         type="search"
         className="block p-2.5 w-full  text-sm text-grey-dark  rounded-lg border-s-grey-main  border border-grey-light focus:ring-focus focus:border-ring-focus  "
         placeholder="Search"
+        onChange={(e) => setSearch(e.target.value)}
         required
       />
       <div className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white rounded-e-lg border ">
@@ -16,7 +17,6 @@ export const Search = () => {
           className="w-5 h-5 text-grey-main"
           aria-hidden="true"
         />
-        <span className="sr-only">Search</span>
       </div>
     </div>
   )
